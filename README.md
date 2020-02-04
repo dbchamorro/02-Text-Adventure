6809e6968a8d3b5943119e4bfb46343507caa77b
Destiny C <dbchamorro@yahoo.com>
2020-02-03 20:08:54 -0500

    FINAL ZOMBIE CODE

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 02-Text-Adventure
 The purpose of this program is to create a game engine that can generate an Interactive fiction game based on eternal files. 
+
+I wrote the zombie.json code. The story isnt that long or detailed but it fits the requirements for location and use. I also took inspiration from the Walking Dead!
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 # The game and item description files (in the same folder as this script)
 game_file = 'zork.json'
 item_file = 'items.json'
-current = ['BUNKER','BACKROOM','ZOMROOM']
+current = ['BUNKER','BACKROOM','ZOMROOM','FRSTDEST']
 
 def load_files():
     try:
